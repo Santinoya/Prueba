6815638e582c035ff8cf4cde4aaf9b00020c736b
Santinoya <santiagonoyaa@gmail.com>
2021-06-02 20:12:26 -0300

    prueba 3

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ saludo1 = "hola"
 nombre = input("Ingrese su nombre:")
 print(saludo1 + nombre )
 '''
-
 num1 = int(input("Ingrese un numero"))
 num2 = int(input("Ingrese un numero"))
 print (num1 * num2)
-
